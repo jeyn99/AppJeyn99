58d7af071b401c81764f19774661b867a07a2291
jeyn99 <jane.repollo@student.passerellesnumeriques.org>
2019-09-02 10:44:30 +0800

    To be check

--- controller.js
+++ controller.js
@@ -1,34 +1,51 @@
 $(document).ready(function () {
+  function addRow(name, flag, ccode, population) {
+    var row = "<tr><td>" + name + "</td><td><img src=" + flag + ">" + "</img></td><td>" + ccode + "</td><td>" + population + "</td></tr>";
+    $("tbody").append(row);
+  };
 
+  function emptyTable() {
+    $("#countryDetails").empty();
+  };
+
+  $(document).ajaxStart(function () {
+    $("#load").show();
+    $("#btnSearch").attr("disable", true);
+    $("#btnSearch").text("Searching")
+  });
+  $(document).ajaxStop(function () {
+    $("#load").hide();
+    $("#btnSearch").attr("disable", false);
+    $("#btnSearch").text("Search")
+  });
   
   $("#btnSearch").click(function () {
-    
-      $(document).ajaxStart(function () {
-        $("#load").show();
-        $("#btnSearch").attr("disable", true);
-        $("#btnSearch").text("Searching")
-      });
-
-      $(document).ajaxStop(function () {
-        $("#load").hide();
-        $("#btnSearch").attr("disable", false);
-        $("#btnSearch").text("Search")
-      });
     var input = $("#countryName").val().toUpperCase();
     emptyTable();
     var apiLinkDS = "https://restcountries.eu/rest/v2/name/" + input;
+    var name;
+    var flag;
+    var code;
+    var pop;
+    var status = false;
 
-    $.ajax({
+    $.get({
       url: apiLinkDS,
       crossDomain: true,
       method: "GET",
       error: function(error) {
-        alert("No results Found")
+        alert("No results!")
       },
       success: function (data) {
-        for(let i = 0 ; i > data.length ;++i){
-          if(data[i].name.toUpperCase().substring(0 , input.length) == input){
-            addRow(data[i].name, data[i].flag, data[i].code, data[i].population)
+
+        for (let i = 0; i < data.length; ++i) {
+          name = (data[i].name).toUpperCase();
+          code = (data[i].alpha2Code).toUpperCase();
+          if (name.substring(0, input.length) == input || code == input) {
+            flag = (data[i].flag);
+            pop = (data[i].population);
+            addRow(name, flag, code, pop);
+            $("table").show();
           }
         }
       }      
